087d70fbbc96660a7b278cf432b5ecdc1d1e8637
Андрей Я <morphingsleep@gmail.com>
2018-07-24 21:37:32 +0700

    Create admin dahboard

--- app/Http/Controllers/Admin/DashbroadController.php
+++ app/Http/Controllers/Admin/DashbroadController.php
@@ -7,5 +7,10 @@ use App\Http\Controllers\Controller;
 
 class DashbroadController extends Controller
 {
-    //
+    //Dashboard
+    public function dashboard()
+    {
+      // code...
+      return view('admin.dashboard');
+    }
 }
